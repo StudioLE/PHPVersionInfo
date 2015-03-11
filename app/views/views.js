6a9ec6cb728d5d63f53d940a8b1519b2c125c39c
Laurence Elsdon <git@elsdon.me>
2015-03-11 16:13:14 +0000

    Introduced ChartService

--- app/views/views.js
+++ app/views/views.js
@@ -88,6 +88,152 @@ angular.module('myApp.views', ['ngRoute', 'highcharts-ng'])
 
 /*****************************************************************
 *
+* Chart factory
+*
+******************************************************************/
+.factory('ChartService', function(ReleasesService) {
+  return {
+    create: function(hosts, phpReleases) {
+
+      // Initialise our data object. Create a key for each version
+      var data = {}
+      var versions = ReleasesService.versions
+      versions.forEach(function(ver) { data[ver] = {} })
+
+      // Go through the list of hosts and add the host versions into the data object
+      hosts.forEach(function(host) {
+        versions.forEach(function(ver) {
+          if(host[ver] === undefined ||
+              host[ver] === '??') {
+            // If the version if unknown do nothing
+          }
+          // If this is the first entry for this version then set some defaults
+          else if( ! data[ver]['v' + host[ver]]) {
+            // If we don't have any relase information
+            if(phpReleases[ReleasesService.format(ver, host[ver])] === undefined) {
+              console.debug('No release information for version ' + ReleasesService.format(ver, host[ver]))
+            }
+            // If we don't have a relase date
+            else if(phpReleases[ReleasesService.format(ver, host[ver])]['date'] === undefined) {
+              console.debug('No release date for version ' + ReleasesService.format(ver, host[ver]))
+              // No release dates for 5.5.22, 5.6.6
+              // No release information for 5.2.6
+              // @todo manual override of release date
+            }
+            // If we do have a release date create an entry in the data object
+            else {
+              // Release date
+              // Due to timezones this information may be a day out. This shouldn't be an issue.
+              var rDate = new Date(phpReleases[ReleasesService.format(ver, host[ver])]['date'])
+              var monthsSince = (Date.now() - rDate.valueOf()) / (1000*60*60*24*30)
+
+              // @todo Style tooltips
+              data[ver]['v' + host[ver]] = {
+                name: host.name,
+                version: ReleasesService.format(ver, host[ver]),
+                releaseDate: rDate.toISOString().substr(0,10),
+                x: rDate.valueOf(),
+                y: 1
+              }
+            }
+          }
+          // If this is not the first entry for this version then we append the host name and and ++ the count
+          else {
+            data[ver]['v' + host[ver]].name += ', ' + host.name      
+            data[ver]['v' + host[ver]].y ++
+          }
+        })
+      })
+
+      var hostsData = []
+
+      // Create a series array from our data object
+      versions.forEach(function(ver){
+          var color = false
+          var vis = true
+        if(ver == 'php52') {
+          color = '#BFA365'
+          vis = false
+        }
+        else if(ver == 'php53') {
+          color = '#2C3E50'
+          vis = false
+        }
+        else if(ver == 'php54') {
+          color = '#349872'
+        }
+        else if(ver == 'php55') {
+          color = '#8E70DB'
+        }
+        else if(ver == 'php56') {
+          color = '#2980B9'
+        }
+        else if(ver == 'default') {
+          color = '#E74C3C'
+          vis = false
+        }
+        hostsData.push({
+          name: ReleasesService.format(ver),
+          data: Object.keys(data[ver]).map(function (key) {
+            return data[ver][key]
+          }),
+          visible: vis,
+          color: color
+        })
+      })
+
+      // @todo sort data
+
+      //This is not a highcharts object. It just looks a little like one!
+      return {
+        options: {
+          //This is the Main Highcharts chart config. Any Highchart options are valid here.
+          //will be overriden by values specified below.
+          chart: {
+            type: 'column'
+          },
+          tooltip: {
+            style: {
+                padding: 10,
+                //fontWeight: 'bold'
+            },
+            useHTML: true,
+            headerFormat: '',
+            pointFormat: '<h3>{point.version}</h3><p class="text-muted">Released {point.x:%b %Y}, used by {point.y} hosts</p><p>{point.key}</p>',
+            footerFormat: '<p>{point.key}</p>'
+          },
+          xAxis: {
+            //currentMin: 0,
+            //currentMax: 20,
+            title: {
+              text: 'Version release date',
+            },
+            type: 'datetime',
+            dateTimeLabelFormats: {
+              month: '%b %Y'
+            },
+            reversed: true          
+          },
+          yAxis: {
+            //currentMin: 0,
+            //currentMax: 20,
+            title: {
+              text: 'Number of hosts using version',
+            }
+          }
+        },
+        series: hostsData,
+        title: {
+           text: ''
+        },
+        loading: false
+      };
+    }
+  }
+})
+
+/*****************************************************************
+*
 * IntroCtrl controller
 *
 ******************************************************************/
@@ -102,159 +248,14 @@ angular.module('myApp.views', ['ngRoute', 'highcharts-ng'])
 *
 ******************************************************************/
 
-.controller('SharedCtrl', ['$scope', '$http', 'YamlService', 'ReleasesService', function($scope, $http, YamlService, ReleasesService) {
+.controller('SharedCtrl', ['$scope', 'YamlService', 'ReleasesService', 'ChartService', function($scope, YamlService, ReleasesService, ChartService) {
 
   YamlService.get('shared_hosts').then(function(data){
     $scope.hosts = data
     ReleasesService.get().then(function(data){
-      populateChart(data)
+      $scope.chartConfig = ChartService.create($scope.hosts, data)
     })
   })
-
-  function populateChart(phpReleases) {
-
-    var hosts = $scope.hosts
-
-    // Initialise our data object. Create a key for each version
-    var data = {}
-    var versions = ReleasesService.versions
-    versions.forEach(function(ver) { data[ver] = {} })
-
-    // Go through the list of hosts and add the host versions into the data object
-    hosts.forEach(function(host) {
-      versions.forEach(function(ver) {
-        if(host[ver] === undefined ||
-            host[ver] === '??') {
-          // If the version if unknown do nothing
-        }
-        // If this is the first entry for this version then set some defaults
-        else if( ! data[ver]['v' + host[ver]]) {
-          // If we don't have any relase information
-          if(phpReleases[ReleasesService.format(ver, host[ver])] === undefined) {
-            console.debug('No release information for version ' + ReleasesService.format(ver, host[ver]))
-          }
-          // If we don't have a relase date
-          else if(phpReleases[ReleasesService.format(ver, host[ver])]['date'] === undefined) {
-            console.debug('No release date for version ' + ReleasesService.format(ver, host[ver]))
-            // No release dates for 5.5.22, 5.6.6
-            // No release information for 5.2.6
-            // @todo manual override of release date
-          }
-          // If we do have a release date create an entry in the data object
-          else {
-            // Release date
-            // Due to timezones this information may be a day out. This shouldn't be an issue.
-            var rDate = new Date(phpReleases[ReleasesService.format(ver, host[ver])]['date'])
-            var monthsSince = (Date.now() - rDate.valueOf()) / (1000*60*60*24*30)
-
-            // @todo Style tooltips
-            data[ver]['v' + host[ver]] = {
-              name: host.name,
-              version: ReleasesService.format(ver, host[ver]),
-              releaseDate: rDate.toISOString().substr(0,10),
-              x: rDate.valueOf(),
-              y: 1
-            }
-          }
-        }
-        // If this is not the first entry for this version then we append the host name and and ++ the count
-        else {
-          data[ver]['v' + host[ver]].name += ', ' + host.name      
-          data[ver]['v' + host[ver]].y ++
-        }
-      })
-    })
-
-    var hostsData = []
-
-    // Create a series array from our data object
-    versions.forEach(function(ver){
-        var color = false
-        var vis = true
-      if(ver == 'php52') {
-        color = '#BFA365'
-        vis = false
-      }
-      else if(ver == 'php53') {
-        color = '#2C3E50'
-        vis = false
-      }
-      else if(ver == 'php54') {
-        color = '#349872'
-      }
-      else if(ver == 'php55') {
-        color = '#8E70DB'
-      }
-      else if(ver == 'php56') {
-        color = '#2980B9'
-      }
-      else if(ver == 'default') {
-        color = '#E74C3C'
-        vis = false
-      }
-      hostsData.push({
-        name: ReleasesService.format(ver),
-        data: Object.keys(data[ver]).map(function (key) {
-          return data[ver][key]
-        }),
-        visible: vis,
-        color: color
-      })
-    })
-
-    // @todo sort data
-
-    //This is not a highcharts object. It just looks a little like one!
-    var chartConfig = {
-
-      options: {
-        //This is the Main Highcharts chart config. Any Highchart options are valid here.
-        //will be overriden by values specified below.
-        chart: {
-          type: 'column'
-        },
-        tooltip: {
-          style: {
-              padding: 10,
-              //fontWeight: 'bold'
-          },
-          useHTML: true,
-          headerFormat: '',
-          pointFormat: '<h3>{point.version}</h3><p class="text-muted">Released {point.x:%b %Y}, used by {point.y} hosts</p><p>{point.key}</p>',
-          footerFormat: '<p>{point.key}</p>'
-        },
-        xAxis: {
-          //currentMin: 0,
-          //currentMax: 20,
-          title: {
-            text: 'Version release date',
-          },
-          type: 'datetime',
-          dateTimeLabelFormats: {
-            month: '%b %Y'
-          },
-          reversed: true          
-        },
-        yAxis: {
-          //currentMin: 0,
-          //currentMax: 20,
-          title: {
-            text: 'Number of hosts using version',
-          }
-        }
-      },
-      series: hostsData,
-      title: {
-         text: ''
-      },
-      loading: false
-    };
-    
-    $scope.chartConfig = chartConfig;
-  }
-
-
-   
 
 }])
 
@@ -285,4 +286,5 @@ angular.module('myApp.views', ['ngRoute', 'highcharts-ng'])
   YamlService.get('linux_distros').then(function(data){
     $scope.distros = data
   })
+  
 }])
