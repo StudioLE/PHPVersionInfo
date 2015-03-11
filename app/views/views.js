2ce894bbbf0ca275cf9daeb9e6e9488338b6edc7
Laurence Elsdon <git@elsdon.me>
2015-03-11 01:39:06 +0000

    Chart PHP versions by date of release

--- app/views/views.js
+++ app/views/views.js
@@ -30,12 +30,12 @@ angular.module('myApp.views', ['ngRoute', 'highcharts-ng'])
 .controller('SharedCtrl', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http) {
   
   // Get yaml
+  // @todo Move $get into Hosts factory
   $http.get('https://cdn.rawgit.com/philsturgeon/phpversions.info/gh-pages/_data/shared_hosts.yml')
     .success(function(yaml, status, headers, config) {
       $scope.hosts = jsyaml.safeLoad(yaml)
-      //console.log(hosts)
       if($routeParams.type === 'chart') {
-        populateChart($scope.hosts)
+        getReleases()
       }
     })
     .error(function(data, status, headers, config) {
@@ -50,20 +50,49 @@ angular.module('myApp.views', ['ngRoute', 'highcharts-ng'])
     $scope.partial = 'chart'
   }
 
-  function populateChart(hosts) {
+  function getReleases() {
+    
+    // Get php releases
+    // @todo mechanism to fetch latest php releases from php.net
+    // http://php.net/releases/index.php?serialize=1&version=5&max=100
+    // 'Access-Control-Allow-Origin' not present for php.net so use local JSON
+    // @todo Move $get into Releases factory
+    $http.get('data/releases.json')
+      .success(function(phpReleases, status, headers, config) {
+        if($routeParams.type === 'chart') {
+          populateChart(phpReleases)
+        }
+      })
+      .error(function(data, status, headers, config) {
+        console.error(status)
+        console.error(data)
+      });
+  }
+
+  function populateChart(phpReleases) {
+
+    var hosts = $scope.hosts
 
     var data = {
       php52: {},
       php53: {},
       php54: {},
       php55: {},
-      php56: {}
+      php56: {},
+      default: {}
     }
 
-    var versions = ['php52', 'php53', 'php54', 'php55', 'php56']
+    var versions = ['php52', 'php53', 'php54', 'php55', 'php56', 'default']
 
+    // @todo convert into Service
     function formatVersion(ver, patch) {
-      if(patch === undefined) {
+      if(ver === 'default' && patch === undefined) {
+        return 'default'
+      }
+      else if(ver === 'default') {
+        return patch
+      }
+      else if(patch === undefined) {
         return '5.' + ver.substr(-1)
       }
       else {
@@ -71,57 +100,65 @@ angular.module('myApp.views', ['ngRoute', 'highcharts-ng'])
       }
     }
 
+
     hosts.forEach(function(host) {
       versions.forEach(function(ver) {
-        if(host[ver] === undefined || host[ver] === '??') {
+        if(host[ver] === undefined ||
+            host[ver] === '??') {
           // If unknown do nothing
         }
         else if( ! data[ver]['v' + host[ver]]) {
-          // @todo unserialize http://php.net/releases/index.php?serialize=1 and get age of releases
-          // @todo Style tooltips
-          data[ver]['v' + host[ver]] = {
-            name: formatVersion(ver, host[ver]) + ': ' + host.name,
-            version: formatVersion(ver, host[ver]),
-            x: host[ver],
-            y: 1
+          if(phpReleases[formatVersion(ver, host[ver])] === undefined) {
+            console.debug('No release information for version ' + formatVersion(ver, host[ver]))
+          }
+          else if(phpReleases[formatVersion(ver, host[ver])]['date'] === undefined) {
+            console.debug('No release date for version ' + formatVersion(ver, host[ver]))
+            // No release dates for 5.5.22, 5.6.6
+            // No release information for 5.2.6
+            // @todo manual override of release date
+          }
+          else {
+            // Release date
+            // Due to timezones this information may be a day out. This shouldn't be an issue.
+            var rDate = new Date(phpReleases[formatVersion(ver, host[ver])]['date'])
+            var monthsSince = (Date.now() - rDate.valueOf()) / (1000*60*60*24*30)
+
+            // @todo Style tooltips
+            data[ver]['v' + host[ver]] = {
+              name: host.name,
+              version: formatVersion(ver, host[ver]),
+              releaseDate: rDate.toISOString().substr(0,10),
+              x: rDate.valueOf(),
+              y: 1
+            }
           }
         }
         else {
           data[ver]['v' + host[ver]].name += ', ' + host.name      
           data[ver]['v' + host[ver]].y ++
-        }       
+        }
       })
     })
 
     var hostsData = []
 
     versions.forEach(function(ver){
+      if(ver == 'php52' || ver == 'php53') {
+        var vis = false
+      }
+      else {
+        var vis = true
+      }
       hostsData.push({
         name: formatVersion(ver),
         data: Object.keys(data[ver]).map(function (key) {
           return data[ver][key]
-        })
+        }),
+        visible: vis
       })
     })
 
     // @todo sort data
-
-    var splergh = [{
-      name: 'SiteGround, Aruba, Heart, Hetzner, HostGator',
-      //color: '#00FF00',
-      y: 5,
-      x: 100
-    }, {
-      name: 'VidaHost',
-      //color: '#FF00FF',
-      y: 1,
-      x: 500
-    }, {
-      name: 'Another, Thing, With, Options',
-      //color: '#FF00FF',
-      y: 4,
-      x: 400
-    }]
 
     // Age of release against number of hosts
 
@@ -129,56 +166,46 @@ angular.module('myApp.views', ['ngRoute', 'highcharts-ng'])
     var chartConfig = {
 
       options: {
-          //This is the Main Highcharts chart config. Any Highchart options are valid here.
-          //will be overriden by values specified below.
-          chart: {
-              type: 'column'
+        //This is the Main Highcharts chart config. Any Highchart options are valid here.
+        //will be overriden by values specified below.
+        chart: {
+          type: 'column'
+        },
+        tooltip: {
+          style: {
+              padding: 10,
+              //fontWeight: 'bold'
           },
-          tooltip: {
-              style: {
-                  padding: 10,
-                  fontWeight: 'bold'
-              }
+          useHTML: true,
+          headerFormat: '',
+          pointFormat: '<h3>{point.version}</h3><p class="text-muted">Released {point.x:%b %Y}, used by {point.y} hosts</p><p>{point.key}</p>',
+          footerFormat: '<p>{point.key}</p>'
+        },
+        xAxis: {
+          //currentMin: 0,
+          //currentMax: 20,
+          title: {
+            text: 'Version release date',
+          },
+          type: 'datetime',
+          dateTimeLabelFormats: {
+            month: '%b %Y'
+          },
+          reversed: true          
+        },
+        yAxis: {
+          //currentMin: 0,
+          //currentMax: 20,
+          title: {
+            text: 'Count',
           }
+        }
       },
-      //The below properties are watched separately for changes.
-
-      //Series object (optional) - a list of series using normal highcharts series options.
       series: hostsData,
-      //Title configuration (optional)
       title: {
          text: ''
       },
-      //Boolean to control showng loading status on chart (optional)
-      //Could be a string if you want to show specific loading text.
-      loading: false,
-      //Configuration for the xAxis (optional). Currently only one x axis can be dynamically controlled.
-      //properties currentMin and currentMax provied 2-way binding to the chart's maximimum and minimum
-      xAxis: {
-        //currentMin: 0,
-        //currentMax: 20,
-        title: {
-          text: 'Days since release of version',
-        }
-      },
-      yAxis: {
-        //currentMin: 0,
-        //currentMax: 20,
-        title: {
-          text: 'Count',
-        }
-      },
-      //Whether to use HighStocks instead of HighCharts (optional). Defaults to false.
-      useHighStocks: false,
-      //size (optional) if left out the chart will default to size of the div or something sensible.
-      // size: {
-      //  width: 400,
-      //  height: 300
-      // },
-      //function (optional)
-      // func: function (chart) {
-      //  //setup some logic for the chart
-      // }
+      loading: false
     };
 
 
@@ -199,7 +226,6 @@ angular.module('myApp.views', ['ngRoute', 'highcharts-ng'])
   $http.get('https://cdn.rawgit.com/philsturgeon/phpversions.info/gh-pages/_data/custom_hosts.yml')
     .success(function(yaml, status, headers, config) {
       $scope.hosts = jsyaml.safeLoad(yaml)
-      //console.log(hosts)
     })
     .error(function(data, status, headers, config) {
       console.error(status)
@@ -216,7 +242,6 @@ angular.module('myApp.views', ['ngRoute', 'highcharts-ng'])
   $http.get('https://cdn.rawgit.com/philsturgeon/phpversions.info/gh-pages/_data/linux_distros.yml')
     .success(function(yaml, status, headers, config) {
       $scope.distros = jsyaml.safeLoad(yaml)
-      //console.log(hosts)
     })
     .error(function(data, status, headers, config) {
       console.error(status)
