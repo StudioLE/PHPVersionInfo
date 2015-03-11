28fe8bc81d30f51b2de3e80542273aad5aee877e
Laurence Elsdon <git@elsdon.me>
2015-03-11 20:22:31 +0000

    Split code into more manageable chunks

--- app/views/views.js
+++ app/views/views.js
@@ -55,8 +55,12 @@ angular.module('myApp.views', ['ngRoute', 'highcharts-ng'])
 ******************************************************************/
 .factory('Version', function($http) {
   return {
+    store: {
+      releases: []
+    },
+    data: {},
     versions: [
-      'php52', 'php53', 'php54', 'php55', 'php56', 'default'
+      '5.2', '5.3', '5.4', '5.5', '5.6', 'default'
     ],
     getReleases: function() {
       return $http.get('data/releases.json')
@@ -69,19 +73,136 @@ angular.module('myApp.views', ['ngRoute', 'highcharts-ng'])
           }
         })
     },
+    releases: function(key) {
+      // if(this.store.releases.length >= 1) {
+        return this.store.releases[key]
+      // }
+      // else {
+        // this.getReleases().then(function(data){
+          // console.log(data)
+          // this.store.releases = data 
+          // return data[key]
+        // })
+      // }
+    },
     format: function(ver, patch) {
-      if(ver === 'default' && patch === undefined) {
+      if(ver === 'default' && (patch === undefined || patch === 'flatten')) {
         return 'default'
       }
       else if(ver === 'default') {
         return patch
       }
+      else if(patch === 'flatten') {
+        return 'php' + ver.replace('.', '')
+      }
       else if(patch === undefined) {
-        return '5.' + ver.substr(-1)
+        return ver
       }
       else {
-        return '5.' + ver.substr(-1) + '.' + patch
+        return ver + '.' + patch
       }
+    },
+    getVersionsFromHost: function(hosts) {
+      var parent = this
+      
+      // Create a key for each version in the data object
+      this.versions.forEach(function(ver) {
+        parent.data[ver] = {}
+      })
+
+      // Go through the list of hosts and add the host versions into the data object
+      hosts.forEach(function(host) {
+        parent.versions.forEach(function(ver) {
+
+          // ver = '5.2'
+          // verKey = php52
+          var verKey = parent.format(ver, 'flatten')
+
+          // Access the host object and get key which should be a version patch number
+          var hostVer = host[verKey]
+
+          var verFull = parent.format(ver, hostVer)
+
+          // If the version if unknown do nothing
+          if(hostVer === undefined || hostVer === '??') { }
+          // If a key entry already exists then we append the host name and and ++ the count
+          else if(parent.data[ver][hostVer]) {
+            parent.data[ver][hostVer].name += ', ' + host.name      
+            parent.data[ver][hostVer].y ++
+          }
+          // If we don't have any relase information
+          else if(parent.releases(verFull) === undefined) {
+            console.debug('No release information for version ' + verFull)
+          }
+          // If we don't have a relase date
+          else if(parent.releases(verFull)['date'] === undefined) {
+            console.debug('No release date for version ' + verFull)
+            // No release dates for 5.5.22, 5.6.6
+            // No release information for 5.2.6
+            // @todo manual override of release date
+          }
+          // If this is the first entry for this version then set some defaults
+          // If we do have a release date create an entry in the data object
+          else {
+            // Release date
+            // Due to timezones this information may be a day out. This shouldn't be an issue.
+            var rDate = new Date(parent.releases(verFull)['date'])
+            var monthsSince = (Date.now() - rDate.valueOf()) / (1000*60*60*24*30)
+
+            // @todo Style tooltips
+            parent.data[ver][hostVer] = {
+              name: host.name,
+              version: verFull,
+              releaseDate: rDate.toISOString().substr(0,10),
+              x: rDate.valueOf(),
+              y: 1
+            }
+          }
+        })
+      })
+      return parent.data
+    },
+    getSeriesFromVersions: function() {
+      var parent = this
+      var series = []
+
+      // Create a series array from our data object
+      this.versions.forEach(function(ver){
+          var color = false
+          var vis = true
+        if(ver == '5.2') {
+          color = '#BFA365'
+          vis = false
+        }
+        else if(ver == '5.3') {
+          color = '#2C3E50'
+          vis = false
+        }
+        else if(ver == '5.4') {
+          color = '#349872'
+        }
+        else if(ver == '5.5') {
+          color = '#8E70DB'
+        }
+        else if(ver == '5.6') {
+          color = '#2980B9'
+        }
+        else if(ver == 'default') {
+          color = '#E74C3C'
+          vis = false
+        }
+
+        series.push({
+          name: parent.format(ver),
+          data: Object.keys(parent.data[ver]).map(function (key) {
+            return parent.data[ver][key]
+          }),
+          visible: vis,
+          color: color
+        })
+      })
+
+      return series
     }
   }
 })
@@ -93,141 +214,67 @@ angular.module('myApp.views', ['ngRoute', 'highcharts-ng'])
 ******************************************************************/
 .factory('ChartService', function(Version) {
   return {
-    create: function(hosts, phpReleases) {
-
-      // Initialise our data object. Create a key for each version
-      var data = {}
-      var versions = Version.versions
-      versions.forEach(function(ver) { data[ver] = {} })
-
-      // Go through the list of hosts and add the host versions into the data object
-      hosts.forEach(function(host) {
-        versions.forEach(function(ver) {
-          if(host[ver] === undefined ||
-              host[ver] === '??') {
-            // If the version if unknown do nothing
-          }
-          // If this is the first entry for this version then set some defaults
-          else if( ! data[ver]['v' + host[ver]]) {
-            // If we don't have any relase information
-            if(phpReleases[Version.format(ver, host[ver])] === undefined) {
-              console.debug('No release information for version ' + Version.format(ver, host[ver]))
-            }
-            // If we don't have a relase date
-            else if(phpReleases[Version.format(ver, host[ver])]['date'] === undefined) {
-              console.debug('No release date for version ' + Version.format(ver, host[ver]))
-              // No release dates for 5.5.22, 5.6.6
-              // No release information for 5.2.6
-              // @todo manual override of release date
-            }
-            // If we do have a release date create an entry in the data object
-            else {
-              // Release date
-              // Due to timezones this information may be a day out. This shouldn't be an issue.
-              var rDate = new Date(phpReleases[Version.format(ver, host[ver])]['date'])
-              var monthsSince = (Date.now() - rDate.valueOf()) / (1000*60*60*24*30)
-
-              // @todo Style tooltips
-              data[ver]['v' + host[ver]] = {
-                name: host.name,
-                version: Version.format(ver, host[ver]),
-                releaseDate: rDate.toISOString().substr(0,10),
-                x: rDate.valueOf(),
-                y: 1
-              }
-            }
-          }
-          // If this is not the first entry for this version then we append the host name and and ++ the count
-          else {
-            data[ver]['v' + host[ver]].name += ', ' + host.name      
-            data[ver]['v' + host[ver]].y ++
-          }
-        })
-      })
-
-      var hostsData = []
-
-      // Create a series array from our data object
-      versions.forEach(function(ver){
-          var color = false
-          var vis = true
-        if(ver == 'php52') {
-          color = '#BFA365'
-          vis = false
-        }
-        else if(ver == 'php53') {
-          color = '#2C3E50'
-          vis = false
-        }
-        else if(ver == 'php54') {
-          color = '#349872'
-        }
-        else if(ver == 'php55') {
-          color = '#8E70DB'
-        }
-        else if(ver == 'php56') {
-          color = '#2980B9'
-        }
-        else if(ver == 'default') {
-          color = '#E74C3C'
-          vis = false
-        }
-        hostsData.push({
-          name: Version.format(ver),
-          data: Object.keys(data[ver]).map(function (key) {
-            return data[ver][key]
-          }),
-          visible: vis,
-          color: color
-        })
-      })
-
-      // @todo sort data
-
+    config: {
       //This is not a highcharts object. It just looks a little like one!
-      return {
-        options: {
-          //This is the Main Highcharts chart config. Any Highchart options are valid here.
-          //will be overriden by values specified below.
-          chart: {
-            type: 'column'
+      options: {
+        //This is the Main Highcharts chart config. Any Highchart options are valid here.
+        //will be overriden by values specified below.
+        chart: {
+          type: 'column'
+        },
+        tooltip: {
+          style: {
+              padding: 10,
+              //fontWeight: 'bold'
           },
-          tooltip: {
-            style: {
-                padding: 10,
-                //fontWeight: 'bold'
-            },
-            useHTML: true,
-            headerFormat: '',
-            pointFormat: '<h3>{point.version}</h3><p class="text-muted">Released {point.x:%b %Y}, used by {point.y} hosts</p><p>{point.key}</p>',
-            footerFormat: '<p>{point.key}</p>'
+          useHTML: true,
+          headerFormat: '',
+          pointFormat: '<h3>{point.version}</h3><p class="text-muted">Released {point.x:%b %Y}, used by {point.y} hosts</p><p>{point.key}</p>',
+          footerFormat: '<p>{point.key}</p>'
+        },
+        xAxis: {
+          //currentMin: 0,
+          //currentMax: 20,
+          title: {
+            text: 'Version release date',
           },
-          xAxis: {
-            //currentMin: 0,
-            //currentMax: 20,
-            title: {
-              text: 'Version release date',
-            },
-            type: 'datetime',
-            dateTimeLabelFormats: {
-              month: '%b %Y'
-            },
-            reversed: true          
+          type: 'datetime',
+          dateTimeLabelFormats: {
+            month: '%b %Y'
           },
-          yAxis: {
-            //currentMin: 0,
-            //currentMax: 20,
-            title: {
-              text: 'Number of hosts using version',
-            }
+          reversed: true          
+        },
+        yAxis: {
+          //currentMin: 0,
+          //currentMax: 20,
+          title: {
+            text: 'Number of hosts using version',
           }
-        },
-        series: hostsData,
-        title: {
-           text: ''
-        },
-        loading: false
-      };
+        }
+      },
+      series: [],
+      title: {
+         text: ''
+      },
+      loading: false
+    },
+    create: function(hosts) {
+      var parent = this
+
+      // Fill the releases array
+      return Version.getReleases().then(function(releases) {
+        Version.store.releases = releases
+
+        // Sort the hosts into an object of versions
+        var data = Version.getVersionsFromHost(hosts)
+
+        // Convert the Versions object into a series array
+        parent.config.series = Version.getSeriesFromVersions(data)
+
+        // @todo Insert a mechanism to sort data. Not necessary for Highcharts column chart.
+
+        return parent.config
+      })
     }
   }
 })
@@ -252,8 +299,8 @@ angular.module('myApp.views', ['ngRoute', 'highcharts-ng'])
 
   YamlService.get('shared_hosts').then(function(data){
     $scope.hosts = data
-    Version.getReleases().then(function(data){
-      $scope.chartConfig = ChartService.create($scope.hosts, data)
+    ChartService.create(data).then(function(data){
+      $scope.chartConfig = data
     })
   })
 
